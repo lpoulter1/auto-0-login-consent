d47ef35bb508fab3fd06f50a407643f36bcfd3d5
lpoulter1 <thundercatontheloose@gmail.com>
2024-03-25 13:17:13 +0000

    add form action to redirct

--- src/app/page.js
+++ src/app/page.js
@@ -5,9 +5,11 @@ export default function Home() {
   const searchParams = useSearchParams();
   const auth0_domain = searchParams.get("auth0_domain");
   console.log(auth0_domain);
+  const state = searchParams.get("state");
+  const action = `https://${auth0_domain}/continue?state=${state}`;
   return (
     <main className="flex flex-col items-center justify-between min-h-screen p-24">
-      <form action="" className="flex flex-col gap-4">
+      <form action={action} className="flex flex-col gap-4">
         <label htmlFor="consent" className="flex gap-2">
           I consent
           <input type="checkbox" />
